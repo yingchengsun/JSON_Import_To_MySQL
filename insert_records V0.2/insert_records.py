0dcdd4231d213c089c41d6d194f07a69bf71e1d3
Yingcheng(Jason) Sun <yxs489@case.edu>
2015-11-04 22:41:02 -0500

    Revert "Import data to MySQL from JSON file"
    This reverts commit ea6af3ad0c19d7cf587426f5945affd6db3db057.

--- insert_records V0.2/insert_records.py
+++ insert_records V0.2/insert_records.py
@@ -7,13 +7,15 @@ Created on 09-29-2015
 import MySQLdb
 import json
 
+
 try:
+   
     conn=MySQLdb.connect(host='127.0.0.1',user='root',passwd='****',port=3306)
     cur=conn.cursor()
     
     cur.execute('create database if not exists python')
     conn.select_db('python')
-    cur.execute('create table if not exists patient (city_of_birth varchar(50), first_name varchar(20), last_name varchar(20),\
+    cur.execute('create table if not exists pp (city_of_birth varchar(50), first_name varchar(20), last_name varchar(20),\
     date_of_birth date, gender varchar(20), email varchar(50), phone varchar(20), street varchar(50), \
     postal_code varchar(20), country varchar(20), record_id int (20) not null primary key ,demographics_complete varchar(20), ethnicity varchar(50))\
     DEFAULT CHARSET=utf8 ')
@@ -21,22 +23,25 @@ try:
     fileHandle=file('data.jo')
     fileList = fileHandle.readlines()
     dictinfo = json.loads(fileList[0])
-    
     for patient_record in dictinfo:
-        record_list=[]
-        for record_value in patient_record.values():
+        #record_list=[]
+        for key in patient_record:
             #transfer the coding type 'unicode' to utf8
-            if (type(record_value)==unicode):
-                record_value=record_value.encode('utf-8');
-            record_list.append(record_value)
-        #print len(record_list)
-        cur.execute('insert into patient values(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)',record_list)
+            if (type(key)==unicode):
+                key.encode('utf-8');
+            if (type(patient_record[key])==unicode):
+                patient_record[key].encode('utf-8');
+            #record_list.append(record_value)
+        sql_2 = "insert into pp values(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
+        param=patient_record
+        print len(param)
+        cur.executemany(sql_2,param)  
+        #cur.execute('insert into pp values(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)',record_list)
         conn.commit()
+
     cur.close()
     conn.close()
-    print len(dictinfo),'records have been inserted successfully!'
-    
+
 except MySQLdb.Error,e:
     print "Mysql Error %d: %s" % (e.args[0], e.args[1])
-
 
